c3acc72aff81701d149e42f1aa5196193c4b7447
JuanPerdomo <juandavid.perdomoquiroz@gmail.com>
2022-12-08 20:55:24 -0500

    adding functions with multiple results returned and learn to setting default parameters

--- functions.py
+++ functions.py
@@ -6,3 +6,29 @@ b = int(input('ingresa un segundo número: '))
 
 print(sum(a, b))
 
+print('----------------------------------------------------------------')
+
+def sum_with_range(min, max):
+    print(min, max)
+    suma = 0
+    for x in range(min, max):
+        suma += x
+    return suma
+
+
+result = sum_with_range(1, 10)
+print(result)
+result_2 = sum_with_range(result, result + 10)
+print(result_2)
+
+
+print('----------------------------------------------------------------')
+
+def find_volume(length=1, width=1, depth=1):
+    return length * width * depth, length ** 2, width
+
+resultado, lenngth_exp2, width = find_volume(width=10)
+
+print(resultado)
+print(lenngth_exp2)
+print(width)
